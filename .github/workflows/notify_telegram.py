f2210e0d0e53ac1a3a1440703d433c822dec5910
Ilyasse <46769766+ilyassesalama@users.noreply.github.com>
2024-12-05 01:46:53 +0100

    Update notify_telegram.py

--- .github/workflows/notify_telegram.py
+++ .github/workflows/notify_telegram.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 
-# Switch to the 'host' branch
-subprocess.run(['git', 'checkout', 'host'], check=True)
+# # Switch to the 'host' branch
+# subprocess.run(['git', 'checkout', 'host'], check=True)
 
 # List all files in the current directory
 for root, dirs, files in os.walk('.'):
